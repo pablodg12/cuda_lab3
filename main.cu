90508e401d61dd80b825942ed7dbddabe16a1d0d
pablo ibarra <pabloibarra@MacBook-Pro-de-pablo.local>
2018-12-01 11:55:17 -0300

    first kernel

--- main.cu
+++ main.cu
@@ -6,7 +6,7 @@ __global__ void kernelA(int *A, int *x, int *b, int N){
   int tId = threadIdx.x + blockIdx.x * blockDim.x;
   int mp = tId - 1e4*floor(tId/1e4);
   A[tId] = A[tId]*x[mp];
-  atomicAdd(&b[mp],A[tId]);
+  atomicAdd(b[mp],A[tId]+b[mp]);
 } 
 
 int main(int argc, char const *argv[])
