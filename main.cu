c78630fcd036705294beb73840033bc3439e3549
pablo ibarra <pabloibarra@MacBook-Pro-de-pablo.local>
2018-12-01 11:39:13 -0300

    first kernel

--- main.cu
+++ main.cu
@@ -2,24 +2,40 @@
 #include <math.h>
 
 
+__global__ void kernelA(int *A, int *x, int *b, int N){
+  int tId = threadIdx.x + blockIdx.x * blockDim.x;
+  A[tId] = A[tId]*x[tId%1e4];
+  atomicAdd(&b[tId%1e4],A[tId])
+};
 
 int main(int argc, char const *argv[])
 {
-  int *GPU_x;
+  int *CPU_b; 
   int *GPU_b;
+  int *GPU_x;
   int *GPU_A;
-  cudaMalloc(&GPU_x , 1e4* sizeof(int));
-  cudaMalloc(&GPU_b , 1e4* sizeof(int));
-  cudaMalloc(&GPU_A , 1e8* sizeof(int));
-  cudaMemset(GPU_x, 1, 1e4*sizeof(int));
-  cudaMemset(GPU_A, 1, 1e8*sizeof(int));
 
+  int n = 1e8;
+  int block_size = 256;
+  int grid_size = (int) ceil((float) n/ block_size);
+
+  cudaMalloc(&GPU_x , 1e4 * sizeof(int));
+  cudaMalloc(&GPU_b , 1e4 * sizeof(int));
+  cudaMalloc(&GPU_A , 1e8 * sizeof(int));
+
+  cudaMemset(GPU_x, 1, 1e4 * sizeof(int)); 
+  cudaMemset(GPU_b, 0, 1e4 * sizeof(int));
+  cudaMemset(GPU_A, 1, 1e8 * sizeof(int));
+
+  kernelA<<<grid_size, block_size>>>(GPU_A, GPU_x, GPU_b, n);
+
+  cudaMemcpy(CPU_b, GPU_b, 1e4 * sizeof(int), cudaMemcpyDeviceToHost);
 
   cudaFree(GPU_x);
   cudaFree(GPU_b);
   cudaFree(GPU_A);
 
-
+  printf("%d\n", CPU_b[0]);
 
   return(0);
 }
