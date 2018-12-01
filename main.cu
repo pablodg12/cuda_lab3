e5f0304717b2031cd44e831fe700bfe99d5ceebc
pablo ibarra <pabloibarra@MacBook-Pro-de-pablo.local>
2018-12-01 11:12:10 -0300

    first push

---
+++ main.cu
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+
+
+int main(int argc, char const *argv[])
+{
+  int *GPU_x;
+  int *GPU_b;
+  int *GPU_A;
+  cudaMalloc(&GPU_x , 1e4* sizeof(int));
+  cudaMalloc(&GPU_b , 1e4* sizeof(int));
+  cudaMalloc(&GPU_A , 1e8* sizeof(int));
+  cudaMemset(dataGPU, 1, 1e4*sizeof(int));
+  cudaMemset(dataGPU, 1, 1e8*sizeof(int));
+
+
+  cudaFree(GPU_x);
+  cudaFree(GPU_b);
+  cudaFree(GPU_A);
+
+
+
+  return(0);
+}
