40a588a70b1fbf22cb995ea0022987ea743cb4d8
pablo ibarra <pabloibarra@MacBook-Pro-de-pablo.local>
2018-12-01 14:13:11 -0300

    kernel3

--- kernel3/main.cu
+++ kernel3/main.cu
@@ -40,7 +40,9 @@ int main(int argc, char const *argv[])
 
   cudaMemcpy(CPU_x, GPU_b, 1e4 * sizeof(int), cudaMemcpyDeviceToHost);
 
-  printf("%d\n", CPU_x[0]);
+  for(int k = 0; k< 1e4; k++){
+    printf("%d\n", CPU_x[k]);
+  }
 
   cudaFree(GPU_x);
   cudaFree(GPU_b);
