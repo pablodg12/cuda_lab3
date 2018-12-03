fa309c52649672fe6c8d4dded4c05689083be7b8
pablo ibarra <pabloibarra@MacBook-Pro-de-pablo.local>
2018-12-02 23:27:44 -0300

    hide printf

--- kernel5/main.cu
+++ kernel5/main.cu
@@ -4,14 +4,14 @@
 __global__ void kernelSM(int *A, int *x, int *b, int N){
   extern __shared__ int sm[];
   int tId = threadIdx.x + blockIdx.x * blockDim.x;
-  int b_local = 0;
   for(int k = 0; k < N/blockDim.x+1; k++){
     int v_max = (k == (int) N/blockDim.x? 16:256);
     if(threadIdx.x < v_max){
       sm[threadIdx.x] = x[threadIdx.x + 256*k];
     }
     __syncthreads();
-    if(tId < N){      
+    if(tId < N){    
+      int b_local = 0;  
       for(int t = 0; t < v_max; t++){
         b_local += A[(int)(tId*N+(t+256*k))]*sm[t];
       }
