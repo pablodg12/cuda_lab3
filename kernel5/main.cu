cba46503944583bb29bf91192e2bfe3d2ffcb9c8
pablo ibarra <pabloibarra@MacBook-Pro-de-pablo.local>
2018-12-02 23:31:48 -0300

    hide printf

--- kernel5/main.cu
+++ kernel5/main.cu
@@ -4,19 +4,21 @@
 __global__ void kernelSM(int *A, int *x, int *b, int N){
   extern __shared__ int sm[];
   int tId = threadIdx.x + blockIdx.x * blockDim.x;
+  int b_local = 0; 
   for(int k = 0; k < N/blockDim.x+1; k++){
     int v_max = (k == (int) N/blockDim.x? 16:256);
     if(threadIdx.x < v_max){
       sm[threadIdx.x] = x[threadIdx.x + 256*k];
     }
     __syncthreads();
-    if(tId < N){    
-      int b_local = 0;  
+    if(tId < N){     
       for(int t = 0; t < v_max; t++){
         b_local += A[(int)(tId*N+(t+256*k))]*sm[t];
       }
-      b[tId] += b_local;
     }
+  }
+  if(tId < N){
+    b[tId] += b_local;
   }
 }
 
@@ -52,9 +54,9 @@ int main(int argc, char const *argv[])
 
   cudaMemcpy(CPU_x, GPU_b, 1e4 * sizeof(int), cudaMemcpyDeviceToHost);
 
-  //for(int k = 0; k< 1e4; k++){
-  //  printf("%d\n", CPU_x[k]);
-  //  }
+  for(int k = 0; k< 1e4; k++){
+    printf("%d\n", CPU_x[k]);
+    }
 
   cudaFree(GPU_x);
   cudaFree(GPU_b);
